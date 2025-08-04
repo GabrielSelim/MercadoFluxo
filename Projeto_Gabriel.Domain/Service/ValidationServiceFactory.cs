5560a2927017c85feaeb602caa6dd237511cc801
Gabriel Sanz <gabriel.sanz@infortechms.com.br>
2025-08-04 14:50:34 -0400

    Refatoração e atualização de código e banco de dados
    As alterações incluem a remoção de métodos e propriedades em várias classes de implementação de negócios e suas interfaces, como `ContatoBusinessImplementacao` e `IContatoBusiness`. Vários DTOs, como `ContatoDbo`, tiveram suas propriedades modificadas, impactando a manipulação de dados.
    
    As classes de validação foram atualizadas para refletir as mudanças nas entidades, e o mapeamento para o banco de dados foi ajustado. O código de controle foi modificado para lidar com as novas implementações e inclui melhorias no tratamento de exceções e logs.
    
    A estrutura do projeto foi reorganizada, com novos diretórios e a atualização do arquivo de contexto do banco de dados, `MySQLContext`, para incluir novas entidades. O projeto também foi atualizado para a versão mais recente do Entity Framework Core.
    
    Além disso, foram feitas alterações no arquivo de projeto para incluir novos diretórios e a exclusão de arquivos de imagem que não são mais necessários. As alterações em arquivos SQL incluem a criação de novas tabelas e a adição de colunas para gerenciar permissões de usuários e controle de registros.

--- Projeto_Gabriel.Domain/Service/ValidationServiceFactory.cs
+++ Projeto_Gabriel.Domain/Service/ValidationServiceFactory.cs
@@ -13,38 +13,8 @@ namespace Projeto_Gabriel.Infrastructure.Services.Validation
     {
         public static IEntityValidationService<T> GetValidationService<T>()
         {
-            if (typeof(T) == typeof(Livros))
-                return (IEntityValidationService<T>)new LivrosValidationService();
-
             if (typeof(T) == typeof(Usuario))
                 return (IEntityValidationService<T>)new UsuarioValidationService();
-
-            if (typeof(T) == typeof(Pessoa))
-                return (IEntityValidationService<T>)new PessoaValidationService();
-
-            if (typeof(T) == typeof(CartaPokemon))
-                return (IEntityValidationService<T>)new CartaPokemonValidationService();
-
-            if (typeof(T) == typeof(Cartao))
-                return (IEntityValidationService<T>)new CartaoValidationService();
-
-            if (typeof(T) == typeof(Categoria))
-                return (IEntityValidationService<T>)new CategoriaValidationService();
-
-            if (typeof(T) == typeof(Lancamento))
-                return (IEntityValidationService<T>)new LancamentoValidationService();
-
-            if (typeof(T) == typeof(Parcelamento))
-                return (IEntityValidationService<T>)new ParcelamentoValidationService();
-
-            if (typeof(T) == typeof(ParcelamentoMensal))
-                return (IEntityValidationService<T>)new ParcelamentoMensalValidationService();
-
-            if (typeof(T) == typeof(PessoaConta))
-                return (IEntityValidationService<T>)new PessoaContaValidationService();
-
-            if (typeof(T) == typeof(Contato))
-                return (IEntityValidationService<T>)new ContatoValidationService();
 
             if (typeof(T) == typeof(LogEntry))
                 return (IEntityValidationService<T>)new LogsValidationService();
--- Projeto_Gabriel/db/migrations/V1__Create_Table_Usuarios.sql
+++ Projeto_Gabriel/db/migrations/V1__Create_Table_Usuarios.sql
@@ -5,6 +5,7 @@
 	`full_name` VARCHAR(120) NOT NULL,
 	`refresh_token` VARCHAR(500) NULL DEFAULT '0',
 	`refresh_token_expiry_time` DATETIME NULL DEFAULT NULL,
+	`role` VARCHAR(50) NOT NULL DEFAULT 'User',
 	PRIMARY KEY (`id`),
 	UNIQUE `user_name` (`user_name`)
 )
